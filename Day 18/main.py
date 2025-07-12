c7ea6208c342699de96bd0b12e70438a06f5583c
ShadowSharingan <achar.samarth@gmail.com>
2025-07-12 18:01:30 +0100

    Changes Made

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,12 +1,12 @@
 from turtle import Turtle, Screen
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
 
 # Challenge 1
 for i in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+    timmy.forward(100)
+    timmy.right(90)
 
 
 
