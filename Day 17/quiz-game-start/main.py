1576029953b0525dfe97d411dc48995b9f341328
ShadowSharingan <achar.samarth@gmail.com>
2025-07-12 17:44:21 +0100

    Changes Made

--- Day 17/quiz-game-start/main.py
+++ Day 17/quiz-game-start/main.py
@@ -8,4 +8,10 @@ for i in question_data:
 
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{len(question_bank)}")
