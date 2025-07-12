a4022988657933f06109f0d4ea9d0714a6bf0c24
ShadowSharingan <achar.samarth@gmail.com>
2025-07-12 18:11:27 +0100

    Changes Made

--- Day 18/main.py
+++ Day 18/main.py
@@ -9,8 +9,13 @@ for i in range(4):
     timmy.right(90)
 
 
-
-
+# Types of import
+# import turtle
+# from turtle import Turtle
+# from turtle import * (Imports everything)
+# import turtle as t (imports things with a speified name)
+# import heroes (have to install, so click on heroes, click on red lightbulb, click on install package
+# print(heroes.gen())
 
 
 
