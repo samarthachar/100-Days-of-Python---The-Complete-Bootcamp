5c9ba0fcae9971ec180917a14f59def4d34cbf40
ShadowSharingan <achar.samarth@gmail.com>
2025-07-12 15:56:12 +0100

    Changes Made

--- Day 17/quiz-game-start/main.py
+++ Day 17/quiz-game-start/main.py
@@ -1,7 +1,11 @@
 from question_model import Question
 from data import question_data
-
+from quiz_brain import QuizBrain
 question_bank = []
 for i in question_data:
     question = Question(i["text"], i["answer"])
     question_bank.append(question)
+
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
