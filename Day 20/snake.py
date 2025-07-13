82e8c6b8b925639d6b276ad042d08b40d65f8b25
ShadowSharingan <achar.samarth@gmail.com>
2025-07-13 15:30:21 +0100

    Changes made

--- Day 20/snake.py
+++ Day 20/snake.py
@@ -1,10 +1,15 @@
 from turtle import Turtle
 STARTING_POSITIONS = [(0,0),(0,-20),(0,-40)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
 class Snake:
     def __init__(self):
         self.squares = []
         self.create_snake()
+        self.head = self.squares[0]
     def create_snake(self):
         for position in STARTING_POSITIONS:
             new_square = Turtle("square")
@@ -19,5 +24,17 @@ class Snake:
             new_y = self.squares[sq_num - 1].ycor()
             self.squares[sq_num].goto(new_x, new_y)
         self.squares[0].forward(MOVE_DISTANCE)
-        self.squares[0].left(90)
 
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
