0583029ef722c5bd0a9157a988c25ee88aa198f6
ShadowSharingan <achar.samarth@gmail.com>
2025-07-13 14:35:07 +0100

    Changes made

---
+++ Day 20/Challenge 1.py
@@ -0,0 +1,29 @@
+from turtle import Screen, Turtle
+screen = Screen()
+screen.setup(height=600, width=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+xpos = 0
+for _ in range(0,3):
+    new_square = Turtle("square")
+    new_square.color("white")
+    new_square.penup()
+    new_square.goto(x=xpos,y=0)
+    xpos -= 20
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
