3a95a51716e004b42c49cfc77adf8ebdffd12a2d
ShadowSharingan <achar.samarth@gmail.com>
2025-07-17 16:56:23 +0100

    Changes made

--- Day 22/main.py
+++ Day 22/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
-
+from ball import Ball
+import time
 screen = Screen()
 screen.setup(width= 800, height=600)
 screen.bgcolor("black")
@@ -9,6 +10,8 @@ screen.tracer(0)
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
+
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -19,7 +22,12 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
+    ball.move()
     screen.update()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 
 
 
