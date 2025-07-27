8f89c2eeac40fdc30081970021da870688873305
Samarth <achar.samarth@gmail.com>
2025-07-27 19:48:49 +0100

    Changes made

--- Day 25/US States Game/main.py
+++ Day 25/US States Game/main.py
@@ -1,26 +1,33 @@
-import turtle, pandas
-from turtle import Turtle
+import turtle
+import pandas
 
+# Set up screen
 screen = turtle.Screen()
 screen.title("U.S. States Game")
-image = "blank_states_img.gif"
-screen.addshape(image)
-screen.screensize(491,725)
-turtle.shape(image)
+screen.bgpic("blank_states_img.gif")  # Make sure this image is in your project folder
 
-score = 0
+# Read CSV data
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
-game_is_on = True
+guessed_states = []
 
-while game_is_on:
-    answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="What's another state's name?" )
-    if answer_state in all_states:
-        score += 1
-        turtle = Turtle()
-        turtle.penup()
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = [state for state in all_states if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in all_states and answer_state not in guessed_states:
+        guessed_states.append(answer_state)
         state_data = data[data.state == answer_state]
-        turtle.goto(state_data.x.item,state_data.y.item)
-        turtle.write(state_data.state)
-
-screen.exitonclick()
+        x = int(state_data.x)
+        y = int(state_data.y)
+        writer = turtle.Turtle()
+        writer.hideturtle()
+        writer.penup()
+        writer.goto(x, y)
+        writer.write(answer_state)
