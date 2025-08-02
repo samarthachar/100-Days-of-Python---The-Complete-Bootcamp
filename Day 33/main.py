a81519db3fb21f0d530a7988d858c02627ec4713
Samarth <achar.samarth@gmail.com>
2025-08-02 19:52:24 +0100

    Changes made

--- Day 33/main.py
+++ Day 33/main.py
@@ -1,1 +1,37 @@
 #API - Application Programming Interface
+# import requests
+#
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+# print(data)
+
+import requests
+from datetime import datetime
+
+
+MY_LAT = 51.398247
+MY_LNG = -0.456652
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+
+print(time_now.hour)
+
