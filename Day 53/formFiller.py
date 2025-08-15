caabac23622024c690c62106631989829c437490
Samarth <achar.samarth@gmail.com>
2025-08-15 17:52:58 +0100

    Changes made

--- Day 53/formFiller.py
+++ Day 53/formFiller.py
@@ -13,3 +13,18 @@ class FormFiller:
 
         time.sleep(2)
 
+        address_input = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+        address_input.send_keys(address)
+
+        price_input = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+        price_input.send_keys(price)
+
+        link_input = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+        link_input.send_keys(link)
+
+        submit_button = driver.find_element(By.CSS_SELECTOR, value='.l4V7wb.Fxmcue')
+        submit_button.click()
+
+for index in range(len(addresses)):
+    bot = FormFiller(addresses[index], prices[index], links[index])
+driver.quit()
