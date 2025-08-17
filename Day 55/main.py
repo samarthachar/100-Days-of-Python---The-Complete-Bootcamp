2467f75923aa9b3fd0e8d6a6d740e25e4e1e61b9
Samarth <achar.samarth@gmail.com>
2025-08-17 19:49:53 +0100

    Changes Made

---
+++ Day 55/main.py
@@ -0,0 +1,38 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+def make_bold(func):
+    def wrapper():
+        return f"<b>{func()}</b>"
+    return wrapper
+
+def make_emphasis(func):
+    def wrapper():
+        return f"<em>{func()}</em>"
+    return wrapper
+
+def make_underlined(func):
+    def wrapper():
+        return f"<u>{func()}</u>"
+    return wrapper
+
+
+@app.route('/')
+def hello_world():
+    return '<h1 style="text-align: center"> Hello World!</h1>'
+
+@app.route('/username/<name>/<int:number>')
+def greet(name, number):
+    return f"hello there {name}, you are {number} years old!"
+
+
+@app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
+def bye():
+    return "Bye!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
