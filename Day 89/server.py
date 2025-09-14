7d6aa4d49147e47f24af46e48496bac48622e116
Samarth <achar.samarth@gmail.com>
2025-09-14 11:22:36 +0100

    Changes made

--- Day 89/server.py
+++ Day 89/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, request, url_for, redirect
 from flask_bootstrap import Bootstrap
 import secrets
 
@@ -8,6 +8,9 @@ from forms import LogInForm, SignUpForm
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 Bootstrap(app)
+
+
+
 
 @app.route('/')
 def home():
@@ -31,5 +34,23 @@ def signup():
     form = SignUpForm()
     return render_template('signup.html', form = form)
 
+@app.route('/todo/<todo_name>')
+def todo(todo_name):
+    todos = ['list']
+    if todo_name in todos:
+        return render_template('todo.html', name = todo_name, data=data)
+    else:
+        abort(404)
+
+@app.route('/todo/<todo_name>/add-todo', methods=["POST"])
+def add_todo(todo_name):
+    title = request.form.get('title')
+    description = request.form.get('description')
+    global data
+    data[title] = description
+    return redirect(url_for('todo', todo_name=todo_name))
+
+data = {}
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
