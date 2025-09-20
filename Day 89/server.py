927e6123e34b69a07b52358ce67c52f3d18f1abd
Samarth <achar.samarth@gmail.com>
2025-09-20 19:51:26 +0100

    Changes made

--- Day 89/server.py
+++ Day 89/server.py
@@ -1,13 +1,33 @@
 from flask import Flask, render_template, abort, request, url_for, redirect, jsonify
 from flask_bootstrap import Bootstrap
 import secrets
-
+from flask import Flask, render_template,redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean, select
 
 from forms import LogInForm, SignUpForm
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 Bootstrap(app)
+
+class Base(DeclarativeBase):
+    pass
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+class Task(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=True)
+    ticked: Mapped[bool] = mapped_column(Boolean, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 
@@ -37,19 +57,28 @@ def signup():
 @app.route('/todo/<todo_name>')
 def todo(todo_name):
     todos = ['Your Todos']
+    data = {}
+    tasks = db.session.query(Task).all()
+    for task in tasks:
+        data[task.id] = (task.title, task.description, task.ticked)
     if todo_name in todos:
-        return render_template('todo.html', name = todo_name, data=data)
+        return render_template('todo.html', name=todo_name, data=data)
     else:
         abort(404)
 
+
 @app.route('/todo/<todo_name>/add-todo', methods=["POST"])
 def add_todo(todo_name):
-    global data, id_no
     title = request.form.get('title')
     description = request.form.get('description')
-    task_id = f"task{id_no}"
-    id_no += 1
-    data[task_id] = (title, description, False)
+    with app.app_context():
+        new_task = Task(
+            title=title,
+            description=description,
+            ticked=False
+        )
+        db.session.add(new_task)
+        db.session.commit()
     return redirect(url_for('todo', todo_name=todo_name))
 
 
@@ -57,22 +86,29 @@ def add_todo(todo_name):
 def check_tick(todo_name):
     json = request.get_json()
     checked_ids = json['checked_ids']
-    for unchecked_id in data.keys():
+    ids = db.session.query(Task.id).all()
+    for unchecked_id in [id_tuple[0] for id_tuple in ids]:
         if unchecked_id not in checked_ids:
-            title, description, _ = data[unchecked_id]
-            data[unchecked_id] = (title, description, False)
+            task = db.session.execute(
+                select(Task).where(Task.id == unchecked_id)
+            ).scalar_one_or_none()
+            task.ticked = False
+            db.session.commit()
     for checked_id in checked_ids:
-        title, description, _ = data[checked_id]
-        data[checked_id] = (title, description, True)
+        task = db.session.execute(
+            select(Task).where(Task.id == checked_id)
+        ).scalar_one_or_none()
+        task.ticked = True
+        db.session.commit()
     return redirect(url_for('todo', todo_name=todo_name))
 
 @app.route("/<todo_name>/delete-todo/<todo_id>")
 def delete_todo(todo_name, todo_id):
-    global data
-    data.pop(todo_id, None)
+    task = db.session.get(Task, todo_id)
+    db.session.delete(task)
+    db.session.commit()
+
     return redirect(url_for('todo', todo_name=todo_name))
-data = {}
-id_no = 0
 
 if __name__ == "__main__":
     app.run(debug=True)
