57f61ecfe3b32a0a558b02a91241b2ae80f7d657
Samarth <achar.samarth@gmail.com>
2025-10-10 17:03:43 +0100

    Changes made

--- Day 92/main.py
+++ Day 92/main.py
@@ -24,16 +24,26 @@ def rgb_to_hex(rgb):
     return '#{:02x}{:02x}{:02x}'.format(int(rgb[0]), int(rgb[1]), int(rgb[2]))
 
 
+
 def run_fetch(image):
     image = image.resize((150, 150))
     data = np.array(image).reshape(-1, 3)
+
     kmeans = KMeans(n_clusters=10)
     kmeans.fit(data)
+
     colours = kmeans.cluster_centers_
-    hex_colours = [rgb_to_hex(colour) for colour in colours]
+    labels = kmeans.labels_
+
+    frequency = {}
+    for label in labels:
+        frequency[label] = frequency.get(label, 0) + 1
+
+    sorted_indices = sorted(frequency, key=frequency.get, reverse=True)
+
+    hex_colours = [rgb_to_hex(colours[i]) for i in sorted_indices]
     return hex_colours
 
-@app.route('/file-uploaded', methods=['POST'])
 @app.route('/file-uploaded', methods=['POST'])
 def file_upload():
     global image
@@ -45,6 +55,9 @@ def file_upload():
         file.stream.seek(0)  # Ensure the stream is at the beginning
         image = Image.open(file.stream)
         image.load()  # Fully load the image before the stream closes
+        image = image.convert("RGB")
+        image.save('./static/test.jpg', format='JPEG')
+
         return redirect(url_for('home'))
     except Exception as e:
         return f"Error opening image: {e}", 500
