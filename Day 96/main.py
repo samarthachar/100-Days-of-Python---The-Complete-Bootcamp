8ce3d89be7f0c153f177e893d24872587a05860b
Samarth <achar.samarth@gmail.com>
2025-10-27 13:24:04 +0000

    Changes made

--- Day 96/main.py
+++ Day 96/main.py
@@ -1,15 +1,16 @@
 import requests
-from flask import Flask
-# app  = Flask(__name__)
-#
-# @app.route("/")
-# def api():
-#     pass
-#
-#
-#
-# if __name__ == "__main__":
-#     app.run(debug=True)
+from flask import Flask, render_template, flash, url_for
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import StringField, SubmitField
+
+app  = Flask(__name__)
+app.secret_key = 'secret_key'
+
+class PostcodeForm(FlaskForm):
+    start = StringField('', render_kw={"placeholder": "Enter start postcode"})
+    end = StringField('', render_kw={"placeholder": "Enter destination postcode"})
+    submit = SubmitField('Get route!')
 
 def format_postcode(postcode):
     if " " in postcode:
@@ -30,32 +31,37 @@ def latandlong(postcode):
     return lat,long
 
 
-start = format_postcode(input("Enter start destination: "))
-end = format_postcode(input("Enter end destination: "))
 
+@app.route("/", methods=["GET", "POST"])
+def api():
+    form = PostcodeForm()
+    if form.validate_on_submit():
 
-start = latandlong(start)
-end = latandlong(end)
+        start = form.start.data
+        end = form.end.data
 
-# Enter start destination: E8 3DY
-# Enter end destination: SE16 7TX
+        start = latandlong(start)
+        end = latandlong(end)
 
+        if not start:
+            flash("Please enter a valid start postcode", "warning")
+            return redirect(url_for('api'))
+        elif not end:
+            flash("Please enter a valid end postcode", "warning")
+            return redirect(url_for('api'))
+        else:
 
+            url=f"https://api.tfl.gov.uk/Journey/JourneyResults/{start[0]},{start[1]}/to/{end[0]},{end[1]}"
+            data = requests.get(url=url).json()
 
-if not start:
-    print(f"Please enter a valid start postcode")
-elif not end:
-    print(f"Please enter a valid end postcode")
-else:
+            if "journeys" not in data:
+                flash("No journey found for your inputs", "warning")
+                return redirect(url_for('api'))
 
-    url=f"https://api.tfl.gov.uk/Journey/JourneyResults/{start[0]},{start[1]}/to/{end[0]},{end[1]}"
-    data = requests.get(url=url).json()
+            legs = data["journeys"][0]["legs"]
+            summaries = [leg["instruction"]["summary"] for leg in legs ]
+            return render_template('index.html', form=form, route=summaries)
+    return render_template("index.html", form = form, route=False)
 
-    if "journeys" not in data:
-        print("No journey found for your inputs.")
-        exit()
-
-    legs = data["journeys"][0]["legs"]
-    summaries = [leg["instruction"]["summary"] for leg in legs ]
-    print(summaries)
-
+if __name__ == "__main__":
+    app.run(debug=True)
